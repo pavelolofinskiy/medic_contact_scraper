892ec14b6602c876bab9a73bff1911b6803fc55f
Pavel <pavelolofinskiy@gmail.com>
2023-09-26 18:07:09 +0300

    finished scraper

--- scraper.py
+++ scraper.py
@@ -1,34 +1,34 @@
-from bs4 import BeautifulSoup
+import csv
 import requests
+from bs4 import BeautifulSoup
+import concurrent.futures
 
-def links_scraper(pages_to_scrape=150):
+def links_scraper(pages_to_scrape=2476):
     base_url = 'https://www.dasoertliche.de/?zvo_ok=0&buc=2249&plz=&quarter=&district=&ciid=&kw=Sport%C3%A4rzte&ci=&kgs=&buab=71100098&zbuab=&form_name=search_nat&recFrom='
     scrape_pages = [1] + [i for i in range(26, pages_to_scrape + 1, 25)]
+
     links = []
-    for page_num in scrape_pages:
-        url = f'{base_url}{page_num}'
-        print(url)
-        response = requests.get(url=url)
-        soup = BeautifulSoup(response.text, 'lxml')
-        parent = soup.find('div', class_='clearfix')
+    with requests.Session() as session:
+        for page_num in scrape_pages:
+            url = f'{base_url}{page_num}'
+            print(url)
+            response = session.get(url=url)
+            soup = BeautifulSoup(response.text, 'lxml')
+            parent = soup.find('div', class_='clearfix')
 
-        link_elements = parent.find_all('a', class_='hitlnk_name')
-        for element in link_elements:
-            link_element = element.get('href')
-            links.append(link_element)
-        return links
-
-links_scraper(pages_to_scrape=150)
-def contact_scraper():
-    links = links_scraper(pages_to_scrape=150)  # Get a list of links
-    scraped_results = []  # Create a list to store scraped results
+            link_elements = parent.find_all('a', class_='hitlnk_name')
+            for element in link_elements:
+                link_element = element.get('href')
+                links.append(link_element)
     
-    for link in links:
-        response = requests.get(url=link)
+    return links
+
+def scrape_contact(link):
+    result = {}
+    with requests.Session() as session:
+        response = session.get(url=link)
         soup = BeautifulSoup(response.text, 'lxml')
         parent = soup.find('div', class_='clearfix detailpage')
-
-        result = {}
 
         title_element = parent.find('div', class_='name').find('h1')
         result['title'] = title_element.text.strip() if title_element else None
@@ -49,16 +49,37 @@ def contact_scraper():
             result['phone_num'] = phone.text
         except (AttributeError, ValueError):
             pass
-    
+            
         address_element = parent.find('div', class_='det_address').next_element
         address_text = address_element.text.strip() if address_element else None
         address_text = address_text.replace('\xa0', ' ')
         result['address'] = address_text
+        
+    return result
 
-        # Check if the result is already in the scraped_results list
-        if result not in scraped_results:
-            scraped_results.append(result)
-    for element in scraped_results:
-        print(element)
+def contact_scraper():
+    links = links_scraper(pages_to_scrape=2476) 
+    scraped_results = []  
+    unique_results = set()  
+    
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = list(executor.map(scrape_contact, links))
+        scraped_results.extend(results)
 
-contact_scraper()
+    for result in scraped_results:
+        result_tuple = tuple(result.items())
+        unique_results.add(result_tuple)
+
+    unique_results = [dict(result_tuple) for result_tuple in unique_results]
+
+    with open('scraped_results.csv', mode='w', newline='', encoding='utf-8') as csv_file:
+        fieldnames = ['title', 'site', 'mail', 'phone_num', 'address']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+        writer.writeheader()  
+
+        for result in unique_results:
+            writer.writerow(result)
+
+if __name__ == "__main__":
+    contact_scraper()
